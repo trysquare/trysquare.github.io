e480a9f083294d90695d86b8363c2664f4f5dee1
aswinavofficial <aswinavofficial@gmail.com>
2017-09-30 11:51:47 +0530

    Coming soon

--- js/main.js
+++ js/main.js
@@ -189,7 +189,7 @@
 	/*----------------------------------------------------*/
 	/* Final Countdown Settings
 	------------------------------------------------------ */
-	var finalDate = '2018/01/01';
+	var finalDate = '2017/11/01';
 
 	$('div#counter').countdown(finalDate)
    	.on('update.countdown', function(event) {
